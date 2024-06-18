ca2480d3499ae41b515b97837949374140323a81
mznrasil <mzn.rasil@gmail.com>
2024-06-18 18:22:43 +0545

    feat: worked on hands on exercise

---
+++ serving-files/002-http-serve-content/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/photo.jpg", handlePhoto)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="/photo.jpg" />`)
+}
+
+func handlePhoto(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("photo.jpg")
+	if err != nil {
+		http.Error(w, "File Not Found", 404)
+		return
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		http.Error(w, "File Not Found", 404)
+		return
+	}
+	http.ServeContent(w, r, info.Name(), info.ModTime(), file)
+}
---
+++ serving-files/003-http-serve-file/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/photo.jpg", handlePhoto)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="/photo.jpg" />`)
+}
+
+func handlePhoto(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "photo.jpg")
+}
