960397ff709ac4ee2abce8e3772b01c0542d4966
mznrasil <mzn.rasil@gmail.com>
2024-06-18 18:22:04 +0545

    feat: worked on hands on exercise

---
+++ serving-files/004-http-file-server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/photo", handlePhoto)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handlePhoto(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="/photo.jpg" />`)
+}
